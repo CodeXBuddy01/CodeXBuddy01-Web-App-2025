af1ebb5d1906bbd85c298ab821082bd64797959f
Rakesh Kumar <codexbuddy01@gmail.com>
2025-04-02 17:15:08 +0530

    Redux Setup

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
@@ -22,7 +24,9 @@ export default function RootLayout({
       <body
         className={roboto_mono.className}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
